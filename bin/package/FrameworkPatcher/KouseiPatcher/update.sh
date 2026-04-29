da5cc74c29abbf883462c7ac6fbee9ce3a4651c9
Chau Vinh Tien <tiencv102006@gmail.com>
2026-04-29 14:15:48 +0700

    update file & boolean install mod

--- bin/package/FrameworkPatcher/KouseiPatcher/update.sh
+++ bin/package/FrameworkPatcher/KouseiPatcher/update.sh
@@ -5,10 +5,12 @@ work_dir=$(pwd)
 source $work_dir/functions.sh
 prop="$work_dir/bin/package/FrameworkPatcher/KouseiPatcher/prop"
 appdir="$work_dir/bin/package/FrameworkPatcher/KouseiPatcher/app"
+kaoriospatch=$(grep "install_toolbox" $work_dir/config.env | cut -d '=' -f 2)
 
+if [[ $kaoriospatch == "true" ]]; then
 bash $work_dir/bin/package/FrameworkPatcher/KouseiPatcher/fakelock_patch.sh
 bash $work_dir/bin/package/FrameworkPatcher/KouseiPatcher/patcher.sh
-
 cp -rf $appdir/KaoriosToolbox $work_dir/build/baserom/images/system/system/priv-app
 cp -rf $appdir/com.kousei.kaorios.xml $work_dir/build/baserom/images/system/system/etc/permissions
 cat $prop/build.prop >> $work_dir/build/baserom/images/system/system/build.prop
+fi
--- bin/package/install.sh
+++ bin/package/install.sh
@@ -4,13 +4,16 @@
 work_dir=$(pwd)
 source $work_dir/bin/ddevice/fetchINFO.sh
 BASE_REGION=$(cat $work_dir/bin/ddevice/rom_region.txt)
+insmod=$(grep "install_mods" $work_dir/config.env | cut -d '=' -f 2)
 
+if [[ $insmod == "true" ]]; then
 if [[ $ROMVERSION == "16.0.5" && $ANDROID_VER == "16" ]]; then
     bash $work_dir/bin/package/OS165/insmod.sh
 elif [[ $ROMVERSION == "16.0.0" || $ROMVERSION == "16.0.1" || $ROMVERSION == "16.0.2" || $ROMVERSION == "16.0.3" && $ANDROID_VER == "16" ]]; then
     bash $work_dir/bin/package/OS160/insmod.sh
 elif [[ $ROMVERSION == "V15.0.0" && $ANDROID_VER == "15" ]]; then
     bash $work_dir/bin/package/OS150/insmod.sh
+fi
 fi
 
 bash $work_dir/bin/package/Universal/insfile.sh
